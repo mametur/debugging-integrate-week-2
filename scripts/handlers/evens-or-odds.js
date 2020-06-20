331e699e255d26ef882f84a6966e7c7925195197
Mamé <mameazad.turgut@gmail.com>
2020-06-20 16:29:08 +0200

    finish integrate-week2

--- scripts/handlers/evens-or-odds.js
+++ scripts/handlers/evens-or-odds.js
@@ -4,16 +4,32 @@
 console.log('--- loading: evens or odds');
 
 function evensOrOddsHandler() {
-  debugger;
-  console.log('\n--- action: sum numbers in range ---');
-  console.log('range:', typeof range, '\n', range);
+	debugger;
+	console.log('\n--- action: sum numbers in range ---');
+	console.log('range:', typeof range, '\n', range);
 
-  // read user input
-  const displayEvens = confirm('would you like to see even numbers?');
-  console.log('displayEvens:', typeof displayEvens, '\n', displayEvens);
+	// read user input
+	const displayEvens = confirm('would you like to see even numbers?');
+	console.log('displayEvens:', typeof displayEvens, '\n', displayEvens);
+	let listedNumbers = '';
+	if (displayEvens) {
+		for (let i = 1; i <= range; i++) {
+			if (!(i % 2)) {
+				listedNumbers += `${i}, `;
+			}
+		}
+	} else {
+		for (let i = 1; i <= range; i++) {
+			if (i % 2) {
+				listedNumbers += `${i}, `;
+			}
+		}
+	}
+	// alert result for the user
+	alert(listedNumbers);
 
-  // perform core logic
-  /* create a comma-separated list if all the evens or odds in the range
+	// perform core logic
+	/* create a comma-separated list if all the evens or odds in the range
     if range is 5 and the user asks for evens:
       "2, 4, "
     if range is 5 and the user asks for odds:
@@ -22,12 +38,5 @@ function evensOrOddsHandler() {
       "2, 4, 6, 8, "
     ...
   */
-  let listedNumbers = '';
-  for (_; _; _) {
-
-  }
-  console.log('listedNumbers:', typeof listedNumbers, '\n', listedNumbers);
-
-  // alert result for the user
-  alert(listedNumbers);
+	console.log('listedNumbers:', typeof listedNumbers, '\n', listedNumbers);
 }
